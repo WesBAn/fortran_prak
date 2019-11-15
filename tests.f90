6175305ab52f4b3b4dee139263b885fcfdb5e5e0
WesBAn <wesban1@gmail.com>
2019-11-15 19:55:50 +0300

    Added tests to rep, prepared tests module

---
+++ tests.f90
@@ -0,0 +1,35 @@
+module tests
+    use assert_func
+
+    private :: test_f
+    private :: test_get_x_from_interval
+    private :: test_get_arr_x
+    private :: test_p_n
+
+    public :: run_tests
+
+contains
+    function test_f()
+        logical test_f
+
+    end function test_f
+
+    function test_get_x_from_interval()
+        logical test_get_x_from_interval
+
+    end function test_get_x_from_interval
+
+    function test_get_arr_x()
+        logical test_get_arr_x
+
+    end function test_get_arr_x
+
+    function test_p_n()
+        logical test_p_n
+
+    end function test_p_n
+
+    function run_tests()
+
+    end function run_tests
+end module tests
