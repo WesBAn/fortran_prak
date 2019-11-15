3b10b435deef58f6ad5c85321cb667171d6d9596
WesBAn <wesban1@gmail.com>
2019-11-15 19:53:11 +0300

    Added assert_func module with file assertions.f90

---
+++ assertions.f90
@@ -0,0 +1,65 @@
+module assert_func
+    private
+    interface u_assert
+       module procedure u_assert_integer4, u_assert_integer8 !u_assert_real4, u_assert_real8,  u_assert_logical
+    end interface u_assert
+
+    public :: u_assert
+
+contains
+    function u_assert_integer4(statement1, statement2)
+        integer(KIND=4) statement1, statement2
+        logical u_assert_integer4
+        if (statement1 == statement2) then
+            u_assert_integer4 = .TRUE.
+        else 
+            print *,'test fallen'
+            u_assert_integer4 = .FALSE.
+        end if
+
+    end function u_assert_integer4
+
+    function u_assert_integer8(statement1, statement2)
+        integer(KIND=8) statement1, statement2
+        logical u_assert_integer8
+        if (statement1 == statement2) then
+            u_assert_integer8 = .TRUE.
+        else 
+            print *,'test fallen'
+            u_assert_integer8 = .FALSE.
+        end if
+    end function u_assert_integer8
+    
+    function u_assert_real4(statement1, statement2)
+        real(KIND=4) statement1, statement2
+        logical u_assert_real4
+        if (statement1 == statement2) then
+            u_assert_real4 = .TRUE.
+        else 
+            print *,'test fallen'
+            u_assert_real4 = .FALSE.
+        end if
+    end function u_assert_real4
+
+    function u_assert_real8(statement1, statement2)
+        real(KIND=8) statement1, statement2
+        logical u_assert_real8
+        if (statement1 == statement2) then
+            u_assert_real8 = .TRUE.
+        else 
+            print *,'test fallen'
+            u_assert_real8 = .FALSE.
+        end if
+    end function u_assert_real8
+    
+    function u_assert_logical(statement1, statement2)
+        logical statement1, statement2
+        logical u_assert_logical
+        if (statement1 .eqv. statement2) then
+            u_assert_logical = .TRUE.
+        else 
+            print *,'test fallen'
+            u_assert_logical = .FALSE.
+        end if
+    end function u_assert_logical
+end module assert_func
