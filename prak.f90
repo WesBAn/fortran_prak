ec17ba18d853d33364619fa1ed0b524f6504355a
WesBAn <wesban1@gmail.com>
2019-11-15 20:00:45 +0300

    Added p_n subroutine

--- prak.f90
+++ prak.f90
@@ -1,7 +1,6 @@
 ! author: mc-wesban // Alexander Linnik
 ! prefix big makes letter High bN = N
 
-
 function f(x) 
     real x
     real f
@@ -33,15 +32,36 @@ subroutine get_arr_x(arr_x, c, d, bigN)
 end subroutine get_arr_x
 
 
-subroutine get_arr_p(arr_p, n, x, arr_of_a)
-    integer n, i
-    real ::arr_of_a(0:n)
-    real p, x
-    p = 0.0
+function p_n(n, arr_a, x)
+    integer n
+    real ::arr_a(0:n)
+    real x
+    real p_n
+    
+    real curr_x
+    integer i
+
+    curr_x = 1
+    p_n = 0
     do i = 0, n
-        p = p + arr_of_a(n-i) * (x**i) 
+        p_n = p_n + arr_a(n-i) * curr_x
+        curr_x = curr_x * x
     end do
-end subroutine get_arr_p
+end function p_n
+
+
+! function bigF(n, arr_a, arr_x, bigN)    
+!     integer n
+!     integer ::arr_a(0:n)
+    
+!     real, dimension(vect_size)
+!     real ::bigF = 0
+
+!     integer i
+!     do i = 0, bigN
+!         bigF = bigF + sqr(f(i) )
+!     end do
+! end function bigF
 
 
 program main
@@ -49,7 +69,7 @@ program main
     integer, parameter ::bigN = 25
     integer, parameter ::m = 2
     integer ::i
-    
+
     real, parameter ::c = 0.1
     real, parameter ::d = 1.1
     
@@ -61,16 +81,3 @@ program main
         print '(1x, F10.3)', arr_x(i)
     end do
 end program main
-
-
-! function bigF(vect, vect_size, N)
-!     integer N
-!     integer vect_size
-!     integer i
-!     real, dimension(vect_size) :: vect
-!     real ::bigF = 0
-!     do i = 1, N
-!         bigF = bigF + sqr(f(i) )
-!     end do
-! end function bigF
-
